75795992ad1dc5832192aeecddd08d2e41800ab1
Simon Gaeremynck <gaeremyncks@gmail.com>
2014-09-12 11:25:55 +0100

    Added redirectUrl to shib tenant redirect

--- release.sh
+++ release.sh
@@ -5,4 +5,7 @@ read -p "Github password: " passw; echo
 stty echo
 read -p "Release type (major, minor or patch): " release
 
-GITHUB_USERNAME="$uname" GITHUB_PASSWORD="$passw" grunt "release:$release"
+export GITHUB_USERNAME="$uname"
+export GITHUB_PASSWORD="$passw"
+
+grunt "release:$release"
