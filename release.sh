c7b0e3c439d682ec7dd9c90c89c2ed463d58c23d
Simon Gaeremynck <gaeremyncks@gmail.com>
2014-11-24 14:44:46 +0000

    Added  and using  in release script

--- release.sh
+++ release.sh
@@ -3,9 +3,9 @@ read -p "Github username: " uname
 stty -echo
 read -p "Github password: " passw; echo
 stty echo
-read -p "Release type (major, minor or patch): " release
+read -p "Release type (major, minor, patch or prerelease): " release
 
 export GITHUB_USERNAME="$uname"
 export GITHUB_PASSWORD="$passw"
 
-grunt "release:$release"
+grunt "release-version:$release"
